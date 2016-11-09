7efe377b244b5648134e3a4e348817e4f48872ed
KingCQ <wangchengqvan@hotmail.com>
2016-11-09 19:56:42 +0800

    open some property

--- AwsomeTextField/Class/AwsomeTextField.swift
+++ AwsomeTextField/Class/AwsomeTextField.swift
@@ -8,32 +8,49 @@
 
 import UIKit
 
-class AwsomeTextField: UITextField {
+open class AwsomeTextField: UITextField {
+    open var activeborderColor: UIColor = .blue {
+        didSet {
+            updateborder()
+        }
+    }
+    
+    open var borderColor: UIColor = .gray {
+        didSet {
+            updateborder()
+        }
+    }
+    
+    open var placeholderColor: UIColor = .gray {
+        didSet {
+            updateplaceholder()
+        }
+    }
+    
+    open var placeholderFontScale: CGFloat = 0.7
+    
     var placeholderLabel = UILabel()
     var shapelayer: CAShapeLayer!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         placeholderLabel = UILabel(frame: CGRect(x: 10, y: 0, width: bounds.width - 10, height: bounds.height))
-        let path = UIBezierPath(roundedRect: bounds, cornerRadius: 8)
+        let path = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.height / 4)
         leftViewMode = .always
         leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 20))
-        placeholderLabel.textColor = UIColor.gray
         shapelayer = CAShapeLayer()
         shapelayer.path = path.cgPath
-        shapelayer.fillColor = UIColor.clear.cgColor
-        shapelayer.strokeColor = UIColor.gray.cgColor
         self.layer.addSublayer(shapelayer)
         addSubview(placeholderLabel)
     }
     
-    override func drawPlaceholder(in rect: CGRect) {
+    override open func drawPlaceholder(in rect: CGRect) {
         super.drawPlaceholder(in: rect)
         placeholderLabel.text = placeholder
         placeholder = nil
     }
     
-    override func willMove(toSuperview newSuperview: UIView?) {
+    override open func willMove(toSuperview newSuperview: UIView?) {
         if newSuperview != nil {
             NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidEndEditing), name: Notification.Name.UITextFieldTextDidEndEditing, object: self)
             NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidBeginEditing), name: NSNotification.Name.UITextFieldTextDidBeginEditing, object: self)
@@ -50,22 +67,31 @@ class AwsomeTextField: UITextField {
                 self.placeholderLabel.transform = CGAffineTransform(scaleX: 1, y: 1)
                 self.shapelayer.strokeStart = 0
                 self.shapelayer.strokeEnd = 1
-                self.shapelayer.strokeColor = UIColor.gray.cgColor
+                self.shapelayer.strokeColor = self.borderColor.cgColor
             })
         }
     }
     
     func textFieldDidBeginEditing() {
         UIView.animate(withDuration: 0.5) {
-            self.placeholderLabel.frame = CGRect(x: 20, y: -20, width: 60, height: 44)
-            self.placeholderLabel.transform = CGAffineTransform(scaleX: 1, y: 0.8)
+            self.placeholderLabel.frame = CGRect(x: self.bounds.height / 2, y: -20, width: 60, height: 44)
+            self.placeholderLabel.transform = CGAffineTransform(scaleX: 1, y: self.placeholderFontScale)
             self.shapelayer.strokeStart = 0.1
             self.shapelayer.strokeEnd = 1
-            self.shapelayer.strokeColor = UIColor.blue.cgColor
+            self.shapelayer.strokeColor = self.activeborderColor.cgColor
         }
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    func updateborder()  {
+        shapelayer.fillColor = UIColor.clear.cgColor
+        shapelayer.strokeColor = borderColor.cgColor
+    }
+    
+    func updateplaceholder() {
+        placeholderLabel.textColor = placeholderColor
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
--- AwsomeTextField/ViewController.swift
+++ AwsomeTextField/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     var field: AwsomeTextField!
-
+    var namefield: AwsomeTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        field = AwsomeTextField(frame: CGRect(x: 60, y: 200, width: 320, height: 44))
+        namefield = AwsomeTextField(frame: CGRect(x: 60, y: 200, width: 320, height: 44))
+        namefield.placeholderFontScale = 0.8
+        namefield.placeholder = "姓名"
+        
+        field = AwsomeTextField(frame: CGRect(x: 60, y: 280, width: 320, height: 44))
+        field.borderColor = UIColor.black
+        field.activeborderColor = UIColor.red
+        field.placeholderColor = UIColor.green
+        field.placeholderFontScale = 0.6
         field.placeholder = "手机号"
+        
+        view.addSubview(namefield)
         view.addSubview(field)
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         field.resignFirstResponder()
+        namefield.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
